43907cce7a48eb3783d0fec68fd45c37508597fe
Antonio Botelho <antonio@inhames.com>
2020-07-04 16:26:28 +0100

    Updated TravisCI configuration.

--- setup.py
+++ setup.py
@@ -4,10 +4,48 @@ from setuptools import find_packages, setup
 
 
 def read(*paths):
-    """Build a file path from *paths* and return the contents."""
+    """Builds a file path from *paths* and returns the contents."""
     with open(os.path.join(*paths), "r") as f:
         return f.read()
 
+
+#
+# Setup information.
+#
+VERSION = "1.0"
+
+AUTHOR = "Antonio Botelho"
+AUTHOR_EMAIL = "antonio@inhames.com"
+LICENSE = "MIT"
+
+NAME = "business_calendar"
+KEYWORDS = ["date", "datetime"]
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/botant/py-business-calendar/issues",
+    "Source Code": "https://github.com/botant/py-business-calendar",
+}
+
+DESCRIPTION = "Business days including custom work week and holidays."
+
+LONG_DESCRIPTION = read("README.rst") + "\n\n" + read("HISTORY.rst")
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
 INSTALL_REQUIRES = []
 EXTRAS_REQUIRE = {
@@ -21,34 +59,20 @@ EXTRAS_REQUIRE["dev"] = (
 
 
 setup(
-    name="business_calendar",
-    version="1.0",
-    description="Business days including custom work week and holidays.",
-    long_description=(read("README.rst") + "\n\n" + read("HISTORY.rst")),
-    url="https://github.com/botant/py-business-calendar/",
-    license="MIT",
-    author="Antonio Botelho",
-    author_email="antonio@inhames.com",
-    packages=find_packages(),
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=PROJECT_URLS["Source Code"],
+    project_urls=PROJECT_URLS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
+    include_package_data=True,
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
+    classifiers=CLASSIFIERS,
+    options={"bdist_wheel": {"universal": "1"}},
 )
